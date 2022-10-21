98720b50de8f84a2c8187a7f9318e81f54c0edd5
earlgray <earlgray283@gmail.com>
2022-10-21 17:00:29 +0900

    feat: proglang

---
+++ proglang/proglang.go
@@ -0,0 +1,26 @@
+package proglang
+
+import (
+	"fmt"
+	"path"
+)
+
+type Command struct {
+	ID             string
+	CompileCommand string
+	ExecuteCommand string
+	Filename       string
+}
+
+func NewCommand(langID string, tmpDirPath string) *Command {
+	if langID == "c(gcc)" {
+		return &Command{
+			ID:             "c(gcc)",
+			CompileCommand: fmt.Sprintf("gcc -O2 -Wall -Wextra -o Main %s", path.Join(tmpDirPath, "Main.c")),
+			ExecuteCommand: fmt.Sprintf("./%s", path.Join(tmpDirPath, "Main")),
+		}
+	}
+
+	// unreachable
+	return nil
+}
