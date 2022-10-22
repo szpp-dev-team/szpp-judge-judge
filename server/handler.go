0eef13414f8c6f3a096b09be4b26930bf47c8711
mistralll <miyakei20011007@gmail.com>
2022-10-22 23:43:55 +0900

    feat: get submission from GCS and save it.

--- server/handler.go
+++ server/handler.go
@@ -1,9 +1,55 @@
 package server
 
-import "github.com/szpp-dev-team/szpp-judge-judge/model"
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/szpp-dev-team/szpp-judge-judge/model"
+)
 
 func (srv *Server) HandleJudgeRequest(judgeReq *model.JudgeRequest) (*model.JudgeResponse, error) {
-	// ここからジャッジ開始
+	// GCSの準備
+	ctx := context.Background()
+	bkt := srv.gcs.Bucket("szpp-judge")
+
+	// tmp directory 作成
+	dir := filepath.Join("tmp", "submits", judgeReq.SubmitID)
+	os.Chmod(dir, os.ModePerm)
+
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return nil, err
+	}
+
+	// ソースコードをGCPから取得
+	obj := bkt.Object(filepath.Join("submits", judgeReq.SubmitID))
+
+	r, err := obj.NewReader(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+
+	file, err := os.Create(filepath.Join(dir, "Main.cpp"))
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, r)
+	if err != nil {
+		return nil, err
+	}
+
+	// テストケースをGCSから取得
+
+	// ソースコードをコンパイルする
+
+	// ソースコードを全てのテストケースに対して実行する
+
+	// レスポンスを返す
 
 	return nil, nil
 }
