5a1987378427aac611bcdcf261f101c558a4f265
mistralll <miyakei20011007@gmail.com>
2022-10-22 23:52:29 +0900

    feat: add cpp to proglang.

--- proglang/proglang.go
+++ proglang/proglang.go
@@ -20,6 +20,13 @@ func NewCommand(langID string, tmpDirPath string) *Command {
 			ExecuteCommand: fmt.Sprintf("./%s", path.Join(tmpDirPath, "Main")),
 		}
 	}
+	if langID == "cpp" {
+		return &Command{
+			ID:             "cpp",
+			CompileCommand: fmt.Sprintf("g++ -o %s/Main %s > stdout.txt 2>stderr.txt", path.Join(tmpDirPath), path.Join(tmpDirPath, "Main.cpp")),
+			ExecuteCommand: fmt.Sprintf("./%s", path.Join(tmpDirPath, "Main")),
+		}
+	}
 
 	// unreachable
 	return nil
