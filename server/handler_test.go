a89b0966d51bb532f0ab73a5f80e711bd91f366a
mistralll <miyakei20011007@gmail.com>
2022-10-25 17:31:44 +0900

    feat: add test cases.

--- server/handler_test.go
+++ server/handler_test.go
@@ -21,12 +21,54 @@ func TestHandleSubmit(t *testing.T) {
 
 	srv := New(gcs)
 
-	req := &model.JudgeRequest{SubmitID: "test", TaskID: "test", LanguageID: "cpp", TestcaseIDs: []string{"sample1.txt", "sample2.txt", "sample3.txt"}}
+	testCaseIDs := []string{"sample1.txt", "sample2.txt", "sample3.txt", "sample4.txt", "sample5.txt", "sample6.txt",
+							"sample7.txt", "sample8.txt", "sample9.txt","sample10.txt", "sample11.txt", "sample12.txt"}
 
+	fmt.Println("######## AC ########")	
+	req := &model.JudgeRequest{SubmitID: "test", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
 	res, err := srv.HandleJudgeRequest(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Println(res)
+	fmt.Println("######## AC ########")
+
+	fmt.Println("######## WA ########")
+	req = &model.JudgeRequest{SubmitID: "test-wa", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
+	res, err = srv.HandleJudgeRequest(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+	fmt.Println("######## WA ########")
+
+	// これ失敗する
+	fmt.Println("######## MLE ########")
+	req = &model.JudgeRequest{SubmitID: "test-mle", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
+	res, err = srv.HandleJudgeRequest(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+	fmt.Println("######## MLE ########")
+
+	// これ失敗する
+	fmt.Println("######## TLE ########")
+	req = &model.JudgeRequest{SubmitID: "test-tle", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
+	res, err = srv.HandleJudgeRequest(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+	fmt.Println("######## TLE ########")
+
+	// これ失敗する
+	fmt.Println("######## CE ########")
+	req = &model.JudgeRequest{SubmitID: "test-ce", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
+	res, err = srv.HandleJudgeRequest(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(res)
+	fmt.Println("######## CE ########")
 }
