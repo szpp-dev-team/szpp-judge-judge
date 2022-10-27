d31f4a06f6399560952de22b857f46c54365d916
mistralll <miyakei20011007@gmail.com>
2022-10-28 00:58:37 +0900

    update: test

--- server/handler_test.go
+++ server/handler_test.go
@@ -12,78 +12,66 @@ import (
 	"google.golang.org/api/option"
 )
 
-func TestHandleSubmit(t *testing.T) {
+func TestSubmitAC(t *testing.T) {
+	if err := testEasily("test"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSubmitWA(t *testing.T) {
+	if err := testEasily("test-wa"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSubmitRE(t *testing.T) {
+	if err := testEasily("test-mle"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSubmitTLE(t *testing.T) {
+	if err := testEasily("test-tle"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSubmitCE(t *testing.T) {
+	if err := testEasily("test-ce"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func TestSubmitAllOdd(t *testing.T) {
+	if err := testEasily("test-all-odd"); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func testEasily(submitID string) error {
+	testCaseIDs := []string{"sample1.txt", "sample2.txt", "sample3.txt", "sample4.txt", "sample5.txt", "sample6.txt",
+		"sample7.txt", "sample8.txt", "sample9.txt", "sample10.txt", "sample11.txt", "sample12.txt"}
+	return testRequest(makeSrv(), submitID, "test", "cpp", testCaseIDs)
+}
+
+func makeSrv() *Server {
 	ctx := context.Background()
 	gcs, err := storage.NewClient(ctx, option.WithCredentialsFile("../credentials.json"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	srv := New(gcs)
+	return New(gcs)
+}
 
-	testCaseIDs := []string{"sample1.txt", "sample2.txt", "sample3.txt", "sample4.txt", "sample5.txt", "sample6.txt",
-							"sample7.txt", "sample8.txt", "sample9.txt","sample10.txt", "sample11.txt", "sample12.txt"}
-
-	fmt.Println("######## AC ########")	
-	req := &model.JudgeRequest{SubmitID: "test", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
+func testRequest(srv *Server, submitID string, taskID string, langID string, testCaseIDs []string) error {
+	fmt.Println("######## " + submitID + " ########")
+	req := &model.JudgeRequest{SubmitID: submitID, TaskID: taskID, LanguageID: langID, TestcaseIDs: testCaseIDs}
 	res, err := srv.HandleJudgeRequest(req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Println(res)
-	fmt.Println("######## AC ########")
-
-	fmt.Println("######## WA ########")
-	req = &model.JudgeRequest{SubmitID: "test-wa", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## WA ########")
-
-	fmt.Println("######## RE ########")
-	req = &model.JudgeRequest{SubmitID: "test-mle", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## RE ########")
-
-	fmt.Println("######## TLE ########")
-	req = &model.JudgeRequest{SubmitID: "test-tle", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## TLE ########")
-
-	fmt.Println("######## CE ########")
-	req = &model.JudgeRequest{SubmitID: "test-ce", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## CE ########")
-
-	fmt.Println("######## 半分WA ########")
-	req = &model.JudgeRequest{SubmitID: "test-all-odd", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## 半分WA ########")
-
-	fmt.Println("######## MLE ########")
-	req = &model.JudgeRequest{SubmitID: "test-mle-3", TaskID: "test", LanguageID: "cpp", TestcaseIDs: testCaseIDs}
-	res, err = srv.HandleJudgeRequest(req)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(res)
-	fmt.Println("######## MLE ########")
+	fmt.Println("######## " + req.SubmitID + " ########")
+	return nil
 }
